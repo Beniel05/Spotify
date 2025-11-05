afbc1aa822bb57700601202bb6c66b2f225bffe0
Beniel Ebenezer J <benielebenezer@gmail.com>
2025-11-05 21:34:38 +0530

    Day-1: Successfully installed and set up Shadcn UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1 className="text-red-500 text-3xl font-bold underline">Hello world!</h1>
+      <Button variant={"outline"}>Button</Button>
     </>
   )
 }
