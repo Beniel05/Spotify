b51cfd1303152468f667f2d31576bf5285e2388e
Beniel Ebenezer J <benielebenezer@gmail.com>
2025-11-08 21:59:39 +0530

    Day-4: Integrated Clerk Express SDK and used middleware (protectRoute, requireAdmin) with next() for admin access control

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { getAdmin } from "../controller/admin.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get('/', getAdmin);
+router.get('/', protectRoute, requireAdmin, createSong); 
+// The work related to 'createSong' will be started in the Day-5 (Tomorrow)
 
 export default router;
